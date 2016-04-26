39051a5d4b91eac3b2b11ada6de510c4964c93b1
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-04-26 23:04:29 +0200

    languages.py modified, inspired by notes.py

--- Python Parsing/languages.py
+++ Python Parsing/languages.py
@@ -18,17 +18,10 @@ f.seek(0) # Positionne le curseur au tout début du fichier
 fields = ['id', 'name', 'code', 'script']
 reader = csv.DictReader(f, dialect='excel-tab', fieldnames=fields)
 
-data = []
+db = DB.Database('localhost','group8','toto123', 'cs322')
+
 for row in reader:
-    script = Parse.nullize(row['script'])
-    if script:
-        script = Parse.booleanize(script)
-    data.append((row['id'],row['name'],row['code'],script))
-    
+	tuple = (row['id'], row['name'], row['code'], row['script'])
+	sql = 'INSERT INTO Languages (id, name, code, script) VALUES (%s, %s, %s, %s)'
+	db.insert(sql, tuple)
 
-# Insert data into Database
-
-# db = DB.Database('db4free.net','group8','toto123', 'cs322')
-#
-# sql = 'INSERT INTO Languages (id, name, code, script) VALUES (%s, %s, %s, %s);'
-# db.insertMany(sql, to_db)
