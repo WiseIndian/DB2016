be9ff0ef6775f4378c367614008162d24893d290
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-05-02 22:35:09 +0200

    2 new funtions load and multiInsert

--- Python Parsing/notes.py
+++ Python Parsing/notes.py
@@ -5,9 +5,11 @@
 ### Parse the notes.csv file, and import data to the MySQL database ###
 #######################################################################
 
-import Database as DB
+
 import Parse as Parse
 import csv
+
+import MySQLdb 
 
 # Parse data from csv file
 
@@ -18,9 +20,31 @@ f.seek(0)
 fields = ['id', 'note']
 reader = csv.DictReader(f, dialect='excel-tab', fieldnames=fields)
 
-db = DB.Database('localhost','group8','toto123', 'cs322')
+db = MySQLdb.connect('localhost','group8',
+	 'toto123', 'cs322', local_infile = 1)
+db.commit()
+cursor = db.cursor()
 
-for row in reader:
-    tuple = (row['id'],row['note'])
-    sql = 'INSERT INTO Notes (id, note) VALUES (%s, %s);'
-    db.insert(sql, tuple)
+
+def multiInsert(): 
+	for row in reader:
+	    tuple = (row['id'],row['note'])
+	    sql = 'INSERT INTO Notes (id, note) VALUES (%s, %s);'
+	    db.insert(sql, tuple)
+
+def loadNotes():#TODO: remove \r from csvs
+	sql = '''
+	LOAD DATA LOCAL INFILE 'Books/notes.csv'
+	INTO TABLE Notes
+	FIELDS
+                  TERMINATED BY '\t'
+                  ENCLOSED BY ''
+                  ESCAPED BY '\\'
+         LINES
+                    STARTING BY ''
+                    TERMINATED BY '\n'
+	'''
+	cursor.execute(sql)
+	
+
+loadNotes()
