dff363d5a9a8f84c34f522a4983cf6dd5e400c7c
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-05-03 18:44:05 +0200

    update deleteAll.sql put semicolon at all ends of sql commands :p)

--- deleteAll.sql
+++ deleteAll.sql
@@ -1,38 +1,37 @@
-
- DELETE FROM Authors                              
- DELETE FROM Authors_temp                         
- DELETE FROM Award_Categories                     
- DELETE FROM Award_Categories_temp                
- DELETE FROM Award_Types                          
- DELETE FROM Award_Types_temp                     
- DELETE FROM Awards                               
- DELETE FROM Awards_temp                          
- DELETE FROM Languages                            
- DELETE FROM Notes                                
- DELETE FROM Publication_Series                   
- DELETE FROM Publication_Series_temp              
- DELETE FROM Publication_is_of_Publication_Series 
- DELETE FROM Publications                         
- DELETE FROM Publications_temp                    
- DELETE FROM Publishers                           
- DELETE FROM Publishers_temp                      
- DELETE FROM Tags                                 
- DELETE FROM Title_Publications                   
- DELETE FROM Title_Series                         
- DELETE FROM Title_Series_temp                    
- DELETE FROM Titles                               
- DELETE FROM Titles_temp                          
- DELETE FROM Webpages                             
- DELETE FROM authors_have_publications            
- DELETE FROM authors_referenced_by                
- DELETE FROM award_categories_referenced_by       
- DELETE FROM award_types_referenced_by            
- DELETE FROM publication_series_referenced_by     
- DELETE FROM publishers_referenced_by             
- DELETE FROM title_has_tag                        
- DELETE FROM title_is_part_of_Title_Series        
- DELETE FROM title_is_reviewed_by                 
- DELETE FROM title_is_translated_in               
- DELETE FROM title_series_referenced_by           
- DELETE FROM title_wins_award                     
+ DELETE FROM Authors                              ;
+ DELETE FROM Authors_temp                         ;
+ DELETE FROM Award_Categories                     ;
+ DELETE FROM Award_Categories_temp                ;
+ DELETE FROM Award_Types                          ;
+ DELETE FROM Award_Types_temp                     ;
+ DELETE FROM Awards                               ;
+ DELETE FROM Awards_temp                          ;
+ DELETE FROM Languages                            ;
+ DELETE FROM Notes                                ;
+ DELETE FROM Publication_Series                   ;
+ DELETE FROM Publication_Series_temp              ;
+ DELETE FROM Publication_is_of_Publication_Series ;
+ DELETE FROM Publications                         ;
+ DELETE FROM Publications_temp                    ;
+ DELETE FROM Publishers                           ;
+ DELETE FROM Publishers_temp                      ;
+ DELETE FROM Tags                                 ;
+ DELETE FROM Title_Publications                   ;
+ DELETE FROM Title_Series                         ;
+ DELETE FROM Title_Series_temp                    ;
+ DELETE FROM Titles                               ;
+ DELETE FROM Titles_temp                          ;
+ DELETE FROM Webpages                             ;
+ DELETE FROM authors_have_publications            ;
+ DELETE FROM authors_referenced_by                ;
+ DELETE FROM award_categories_referenced_by       ;
+ DELETE FROM award_types_referenced_by            ;
+ DELETE FROM publication_series_referenced_by     ;
+ DELETE FROM publishers_referenced_by             ;
+ DELETE FROM title_has_tag                        ;
+ DELETE FROM title_is_part_of_Title_Series        ;
+ DELETE FROM title_is_reviewed_by                 ;
+ DELETE FROM title_is_translated_in               ;
+ DELETE FROM title_series_referenced_by           ;
+ DELETE FROM title_wins_award                     ;
  DELETE FROM titles_referenced_by;
