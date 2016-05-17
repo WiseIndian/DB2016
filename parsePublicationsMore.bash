e550354d650fccb946f08ea04221c58b34cb7db3
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-05-17 18:51:13 +0200

    update parsePublicationsMore to parse currencies

--- parsePublicationsMore.bash
+++ parsePublicationsMore.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PUBLICATION_FILE="publications_rem.csv"
 TAB=$'\t' #http://stackoverflow.com/a/2623007/5432899
 anyGroup=".\+${TAB}"
 anyGroups1=""
@@ -15,11 +16,12 @@ do
 
 #add a columns between price number and currency value to make it easier to import Publications
 currencies="$£"
-regexp='\('"${anyGroups1}"'\)\([0-9]\+\)\(['"${currencies}"'\)'"$anyGroups2"
-subst1='s/'"${regexp}"'/\1\2'"${TAB}"'\3\4/'
-sed -i.backup "${subst1}" publications_rem.csv #for lines with a price
+regexp='\('"${anyGroups1}"'\)\('"${currencies}"'\)\([0-9]\+\)'"$anyGroups2"
+#we invert 2 and 3 because of table and csv difference in ordering of fields
+subst1='s/'"${regexp}"'/\1\3'"${TAB}"'\2\4/' 
+sed -i.backup "${subst1}" "$PUBLICATION_FILE" #for lines with a price
 
 subst2='s/\('"${anyGroups1}"'\)\N\('"${anyGroups2}"'\)/\1\N'"$TAB"'\N\2'
-sed -i.backup2 "${subst2}" publications_rem.csv #for lines without a price
+sed -i.backup2 "${subst2}" "$PUBLICATION_FILE" #for lines without a price
 
 
