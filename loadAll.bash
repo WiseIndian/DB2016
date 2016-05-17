51a819e322a18a8099168e6dd4cc7f2ed285f109
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-05-17 18:49:58 +0200

    fix typos add function clean

--- loadAll.bash
+++ loadAll.bash
@@ -7,11 +7,21 @@ csvLocation=CSV #don't put a directory name containing spaces it won't work
 sqlConnBase="mysql -h localhost -u group8 --password=toto123"
 dbName="cs322"
 
-lsOutput=`ls $csvLocation/*_rem.csv` 
-#testing if $lsOutput is an empty string`man test` for more info on this if
-if [ ! -z ${lsOutput:0:20} ]; then #print 21 first characters of $lsOutput and see if its empty
-	rm "$csvLocation"/*_rem.csv #we want to start with a fresh output from rmRet.sh
-fi
+function sqlConn {
+	sqlLoad="$sqlConnBase $@ $dbName"
+	echo "$sqlLoad"
+	eval "$sqLoad"
+}
+
+function clean {
+	lsOutput=`ls $csvLocation/*_rem.csv` 
+	#testing if $lsOutput is an empty string`man test` for more info on this if
+	if [ ! -z ${lsOutput:0:20} ]; then #print 21 first characters of $lsOutput and see if its empty
+		rm "$csvLocation"/*_rem.csv #we want to start with a fresh output from rmRet.sh
+	fi
+}
+
+clean
 
 sh rmRet.sh #delete ^M and other bullshit carriage return \r...
 
@@ -25,11 +35,7 @@ FIELDS TERMINATED BY '\\t' ENCLOSED BY '' ESCAPED BY '\\\\'
 LINES TERMINATED BY '\\n' STARTING BY '';
 "
 
-function sqlConn {
-	sqlLoad="$sqlConnBase $@ $dbName"
-	echo "$sqlLoad"
-	eval "$sqLoad"
-}
+
 #call this function like:
 #loadTuples "csv1.csv,Table1 csv2.csv,Table2" "<additional-sql-code-to-append-to-loadCommand3>"
 #tuples is "csv1.csv,...,Table2" 
@@ -84,7 +90,7 @@ loadTuples "notes_rem.csv,Notes
  
 sqlConn " < authors.sql" #checked
 
-loadTuples "title_series_rem.csv,Title_Series_temp"
+loadTuples "titles_series_rem.csv,Title_Series_temp"
 sqlConn " < title_series.sql" #checked
 
 loadTuples "titles_rem.csv,Titles_temp"
@@ -98,103 +104,47 @@ loadTuples "award_categories_rem.csv,Award_Categories_temp"
 sqlConn " < awardCategories.sql"
 loadTuples "awards_rem.csv,Awards_temp"
 sqlConn " < awards.sql"
+
 #see http://stackoverflow.com/questions/4202564/how-to-insert-selected-columns-from-csv-file-to-mysql-using-load-data-infile  
 #for following 2 lines of code
 csvColumnsTitleAwards="(@col1, @col2, @col3) set award_id=@col2, title_id=@col3"
 loadTuples "titles_awards_rem.csv,title_wins_award" "$csvColumnsTitleAwards"
+
 loadTuples "tags_rem.csv,Tags titles_tag_rem.csv,title_has_tag publishers_rem.csv,Publishers_temp"
-#	publishers = '''
-#	CREATE TABLE Publishers (
-#	  id INTEGER,
-#	  name VARCHAR(255),
-#	  note TEXT,
-#	  PRIMARY KEY (id)
-#	) ENGINE=InnoDB;'''
-#	createTable(publishers)
-#
-#	publication_series_temp = '''
-#	CREATE TABLE Publication_Series_temp (
-#	  id INTEGER,
-#	  name VARCHAR(255),
-#	  note_id INTEGER,
-#	  PRIMARY KEY (id),
-#	  FOREIGN KEY (note_id) REFERENCES Notes(id) ON DELETE SET NULL
-#	) ENGINE=InnoDB;'''
-#	createTable(publication_series_temp)
-#
-#	publication_series = '''
-#	CREATE TABLE Publication_Series (
-#	  id INTEGER,
-#	  name VARCHAR(255),
-#	  note TEXT,
-#	  PRIMARY KEY (id)
-#	) ENGINE=InnoDB;'''
-#	createTable(publication_series)
-#
-#
-#	publications_temp = '''
-#	CREATE TABLE Publications_temp (
-#	  id INTEGER,
-#	  /* we should use a view/relationship that just describes the many to many relationship
-#	   * and references of Titles id and Publications id, see todoFromDeliv1Feedback file(on github)
-#	   * for more info on how to do it.
-#	   */
-#	  title VARCHAR(255), /*stay closer to definition of the csv file as described in todoFromDeliv1Feedback */
-#	  pb_date DATE,
-#	  publisher_id INTEGER,
-#	  nb_pages INTEGER,
-#	  packaging_type VARCHAR(255),
-#	  publication_type ENUM('ANTHOLOGY', 'COLLECTION', 'MAGAZINE', 'NONFICTION',
-#				 'NOVEL', 'OMNIBUS', 'FANZINE', 'CHAPBOOK'),
-#	  isbn INTEGER,
-#	  cover_img VARCHAR(255),
-#	  price DECIMAL(6, 5), /*valeurs un peu arbitraires, mais on peut imaginer qu'un livre 
-#				 aura pas un prix > un million dans n'importe quelle currency?*/
-#	  currency VARCHAR(1),
-#	  note_id INTEGER,
-#	  PRIMARY KEY (id),
-#	  FOREIGN KEY (note_id) REFERENCES Notes(id) ON DELETE SET NULL,
-#	  FOREIGN KEY (publisher_id) REFERENCES Publishers(id) ON DELETE CASCADE
-#	) ENGINE=InnoDB;'''
-#	createTable(publications_temp)
-#
-#	publications = '''
-#	CREATE TABLE Publications (
-#	  id INTEGER,
-#	  /* we should use a view/relationship that just describes the many to many relationship
-#	   * and references of Titles id and Publications id, see todoFromDeliv1Feedback file(on github)
-#	   * for more info on how to do it.
-#	   */
-#	  title VARCHAR(255), /*stay closer to definition of the csv file as described in todoFromDeliv1Feedback */
-#	  pb_date DATE,
-#	  publisher_id INTEGER,
-#	  nb_pages INTEGER,
-#	  packaging_type VARCHAR(255),
-#	  publication_type ENUM('ANTHOLOGY', 'COLLECTION', 'MAGAZINE', 'NONFICTION',
-#				 'NOVEL', 'OMNIBUS', 'FANZINE', 'CHAPBOOK'),
-#	  isbn INTEGER,
-#	  cover_img VARCHAR(255),
-#	  price DECIMAL(6, 5), /*valeurs un peu arbitraires, mais on peut imaginer qu'un livre 
-#				 aura pas un prix > un million dans n'importe quelle currency?*/
-#	  currency VARCHAR(1),
-#	  note TEXT,
-#	  PRIMARY KEY (id),
-#	  FOREIGN KEY (publisher_id) REFERENCES Publishers(id) ON DELETE CASCADE
-#	) ENGINE=InnoDB;'''
-#	createTable(publications)
-#
-#	publication_is_of_Publication_Series = '''
-#	CREATE TABLE Publication_is_of_Publication_Series (
-#	publication_id  INTEGER,
-#	series_id INTEGER,
-#	series_number INTEGER,
-#	PRIMARY KEY (publication_id),
-#	FOREIGN KEY (publication_id) REFERENCES Publications(id) ON DELETE CASCADE,
-#	FOREIGN KEY (series_id) REFERENCES Publication_Series(id) ON DELETE CASCADE
-#	) ENGINE=InnoDB;'''
-#	createTable(publication_is_of_Publication_Series)
-#
-#
+sqlConn " < publishers.sql"
+loadTuples "publications_series_rem.csv,Publication_Series_temp"
+sqlConn " < publications_series.sql"
+
+bash parsePublicationsMore.bash
+#        publications_temp = '''
+#        CREATE TABLE Publications_temp (
+#          id INTEGER,
+#          title VARCHAR(255), /*stay closer to definition of the csv file as described in todoFromDeliv1Feedback and then we'll use this field to create view Title_Publications*/
+#          pb_date DATE,
+#          publisher_id INTEGER,
+#          nb_pages INTEGER,
+#          packaging_type VARCHAR(255),
+#          publication_type ENUM('ANTHOLOGY', 'COLLECTION', 'MAGAZINE', 'NONFICTION',
+#                                 'NOVEL', 'OMNIBUS', 'FANZINE', 'CHAPBOOK'),
+#          isbn INTEGER,
+#          cover_img VARCHAR(255),
+#          price DECIMAL(6, 5), /*valeurs un peu arbitraires, mais on peut imaginer qu'un livre 
+#                                 aura pas un prix < un million dans n'importe quelle currency?*/
+#          currency VARCHAR(1),
+#          note_id INTEGER,
+#          publication_series_id INTEGER,
+#          publication_series_number INTEGER,
+#          PRIMARY KEY (id),
+#          FOREIGN KEY (note_id) REFERENCES Notes(id) ON DELETE SET NULL,
+#          FOREIGN KEY (publisher_id) REFERENCES Publishers(id) ON DELETE CASCADE,
+#          FOREIGN KEY (publication_series_id) REFERENCES Publications_Series
+#        ) ENGINE=InnoDB;'''
+#        createTable(publications_temp)
+
+
+csvColumnsPublicationAuthors="(@col1, @col2, @col3) set author_id=@col3, pub_id=@col2"
+loadTuples "publications_authors_rem.csv,authors_have_publications" "$csvColumnsPublicationAuthors"
+
 #	authors_have_publications = '''
 #	CREATE TABLE authors_have_publications (
 #	  author_id  INTEGER,
