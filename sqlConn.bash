f0ec51435428bda885c251cf7b5e52e74bb38b3a
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-05-30 20:29:21 +0200

    add some lines to ignore password warning from appearing on terminal

--- sqlConn.bash
+++ sqlConn.bash
@@ -4,10 +4,16 @@ source dbConfig.bash
 
 sqlConnBase="mysql -h $host -u $user -p$password $db"
 
+pWarn="Warning: Using a password on the command line interface can be insecure"
 function sqlConn { 
         sqlLoad="$sqlConnBase $@"
-        echo "$sqlLoad"
-        eval "$sqlLoad"
+        output=`eval "$sqlLoad" 2>stderrFile`
+	cat stderrFile | sed '/^.*'"$pWarn"'.*$/d' 1>&2 ;
+	#the following redirects stderr to a file so that we can
+	#ignore $pWarn which really pollutes the terminal when
+	# we're just trying to debug our program:
+	#"$sqlLoad" 2>stderrFile`
+        #cat stderrFile | sed '/^.*'"$pWarn"'.*$/d' 1>&2 ;
 }
 
 sqlConn "$@"
