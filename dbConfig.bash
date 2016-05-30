922952e4ebf7e460f205986522b26535f9b03c98
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-05-30 19:38:01 +0200

    merge config and dbConfig.bash variables in dbConfig.bash, and remove executable lines from dbCOnfig.bash so that we can safely source dbConfig.bash, move those lines of codes to main.bash

--- dbConfig.bash
+++ dbConfig.bash
@@ -6,9 +6,5 @@ password="toto123"
 db="cs322"
 database="$db"
 
-
-echo "create database $db;
-CREATE USER '$user'@'localhost' IDENTIFIED BY '$password';
-GRANT ALL PRIVILEGES ON $db"".* TO '$user'@'localhost';" > sqlConfTmp.sql
-
-mysql -u root -p < sqlConfTmp.sql 
+needInstall=1
+needConfig=1
--- main.bash
+++ main.bash
@@ -1,14 +1,28 @@
-needInstall=0
-needConfig=1
+configFile=dbConfig.bash
+source "$configFile" #contains needInstall and needConfig...
 
-if [ needInstall == 1 ]
+function replaceConfigVar {
+	# $1 should be the name of the variable in config and $2 its new value
+	varToReplace="$1"
+	newValue="$2"
+	sed -i 's/^.*\('"$varToReplace"'\)=.*/\1='"$newValue"'/g' "$configFile" 
+}
+
+if [ "$needInstall" -eq 1 ]
 then 
+	echo doingInstall
 	sudo bash installSql.bash 
 fi
+replaceConfigVar needInstall 0
 
-if [ needConfig == 1 ] 
+if [ "$needConfig" -eq 1 ] 
 then
-	bash dbConfig.bash
+	echo doingConfig
+	echo "create database $db;
+	CREATE USER '$user'@'localhost' IDENTIFIED BY '$password';
+	GRANT ALL PRIVILEGES ON $db"".* TO '$user'@'localhost';" > sqlConfTmp.sql
+	mysql -u root -p < sqlConfTmp.sql
 fi
+replaceConfigVar needConfig 0
 
-bash loadAll.bash #load all data into the database (can take around 5 minutes)
+#bash loadAll.bash #load all data into the database (can take around 5 minutes)
