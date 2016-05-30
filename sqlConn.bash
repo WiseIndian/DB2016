211c49458093cdce6a766be5982131263a87f4ee
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-05-30 22:40:12 +0200

    do something so that we can also simple use bash sqlConn.bash to connect to our db

--- sqlConn.bash
+++ sqlConn.bash
@@ -6,14 +6,20 @@ sqlConnBase="mysql -h $host -u $user -p$password $db"
 
 pWarn="Warning: Using a password on the command line interface can be insecure"
 function sqlConn { 
-        sqlLoad="$sqlConnBase $@"
-        output=`eval "$sqlLoad" 2>stderrFile`
+	if  [ -z "$@" ]; then
+		sqlCmd="$sqlConnBase $@"
+	else 
+		sqlCmd="$sqlConnBase"
+	fi
+        `eval "$sqlCmd" 2>stderrFile`
 	cat stderrFile | sed '/^.*'"$pWarn"'.*$/d' 1>&2 ;
-	#the following redirects stderr to a file so that we can
+	#the before code redirects stderr to a file so that we can
 	#ignore $pWarn which really pollutes the terminal when
 	# we're just trying to debug our program:
-	#"$sqlLoad" 2>stderrFile`
-        #cat stderrFile | sed '/^.*'"$pWarn"'.*$/d' 1>&2 ;
 }
 
-sqlConn "$@"
+if [ ! -z "$@" ]; then
+	sqlConn "$@"
+else 
+	eval "$sqlConnBase"
+fi
