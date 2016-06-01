44ba0925b5c5844736ba58d07dab801e7b323692
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-06-01 23:16:36 +0200

    change typo title_id into id in title_is_translated_in.sql

--- title_is_translated_in.sql
+++ title_is_translated_in.sql
@@ -1,7 +1,7 @@
 INSERT INTO title_is_translated_in(
 	title_id, trans_title_id, language_id, translator) 
-SELECT parent, title_id, language_id, title_translator
+SELECT parent, id, language_id, title_translator
 FROM Titles_temp
-WHERE parent IS NOT NULL AND title_id IS NOT NULL AND language_id IS NOT NULL
+WHERE parent IS NOT NULL AND id IS NOT NULL AND language_id IS NOT NULL
 AND title_translator IS NOT NULL;
 
