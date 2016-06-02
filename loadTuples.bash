0594fedc8963f29c73f41ce4d7ba231eec482248
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-06-02 19:01:12 +0200

    add bash file for loadTuples function from loadAll

---
+++ loadTuples.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+source dbConfig.bash
+source dbUtils.bash
+
+loadCommand1="LOAD DATA LOCAL INFILE '$csvLocation/"
+#add csv file name in between loadCommand1 and 2 in for loop
+loadCommand2="'
+INTO TABLE " #add a table name here
+loadCommand3="
+CHARACTER SET UTF8
+FIELDS TERMINATED BY '\\t' ENCLOSED BY '' ESCAPED BY '\\\\'
+LINES TERMINATED BY '\\n' STARTING BY '' 
+"
+
+function loadTuples {
+        if [ -f sqlLoadFile.sql ]; then
+                rm sqlLoadFile.sql
+        fi
+
+        tuples="$1"
+        appendable="$2"
+
+        OLDIFS="$IFS"
+        for t in $tuples; do
+                IFS=','
+                set $t
+                base="${loadCommand1}${CSVLoc}${1}${loadCommand2}${2}${loadCommand3}"
+                echo "${base}${appendable};" > sqlLoadFile.sql
+        done
+        IFS=$OLDIFS
+        
+
+        sqlConn "--local-infile=1" '<' sqlLoadFile.sql 
+        #--local-infile=1 option used to be able to import from csv
+        echo "loaded csvs into: $tuples"
+}
+
