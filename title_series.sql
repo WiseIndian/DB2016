72f6a00d8371698cc0165c3071dbbb4bc6a9dc19
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-06-02 22:33:54 +0200

    Add an alter table clause to add foreign key from parent field in title_series to an id in title_series

--- title_series.sql
+++ title_series.sql
@@ -6,3 +6,7 @@ UNION
 SELECT t.id, t.title, t.parent, NULL
 FROM Title_Series_temp t
 WHERE t.note_id IS NULL;
+
+ALTER TABLE Title_Series
+ADD CONSTRAINT fk_parent
+FOREIGN KEY(parent) REFERENCES Title_Series(id);
