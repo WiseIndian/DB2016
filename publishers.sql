46bf7fd4ee88f9fbc1402b0061a99533423a9165
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-06-03 00:42:30 +0200

    some fixes to publishers.sql

--- publishers.sql
+++ publishers.sql
@@ -1,10 +1,10 @@
 INSERT INTO Publishers(id, name, note)
-SELECT pb.id, pb.name, note
+SELECT pb.id, pb.name, n.note
 FROM Publishers_temp pb, Notes n
 WHERE n.id = pb.note_id
 UNION
-SELECT pb.id, pb.name, NULL
-FROM Publishers_temp pb
+SELECT id, name, NULL
+FROM Publishers_temp
 WHERE note_id IS NULL
 ;
 
