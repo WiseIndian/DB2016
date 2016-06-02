646ac5d11017f912af58d0200fbdc375ec6f81f5
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-06-02 19:02:09 +0200

    add a variable for csv locations

--- dbConfig.bash
+++ dbConfig.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+CSVLoc=CSV
+csvLocation="$CSVLoc"
 
 user="group8"
 host="localhost"
@@ -8,4 +10,4 @@ database="$db"
 
 needInstall=0
 needConfig=0
-needCreateTables=0
+needCreateTables=1
