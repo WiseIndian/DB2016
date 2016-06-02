49a12146a56c2b5d10d3e09b49765756ff97a3ac
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-06-02 20:48:45 +0200

    fix > into >> in loadTuples.bash

--- loadTuples.bash
+++ loadTuples.bash
@@ -4,7 +4,6 @@ source dbConfig.bash
 source dbUtils.bash
 
 loadCommand1="LOAD DATA LOCAL INFILE '$csvLocation/"
-echo "load command1: $loadCommand1"
 #add csv file name in between loadCommand1 and 2 in for loop
 loadCommand2="'
 INTO TABLE " #add a table name here
@@ -27,11 +26,12 @@ function loadTuples {
                 IFS=','
                 set $t
                 base="${loadCommand1}${1}${loadCommand2}${2}${loadCommand3}"
-                echo "${base}${appendable};" > sqlLoadFile.sql
+                echo "${base}${appendable};" >> sqlLoadFile.sql
         done
         IFS=$OLDIFS
         
 
+	cat sqlLoadFile.sql
         sqlConn "--local-infile=1" '<' sqlLoadFile.sql 
         #--local-infile=1 option used to be able to import from csv
         echo "loaded csvs into: $tuples"
