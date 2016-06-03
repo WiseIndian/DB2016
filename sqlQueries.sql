42c1cd90dd388f4e6b11762204e04a550db0de4f
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-06-03 09:39:08 +0200

    querry b of deliv 2 done

--- sqlQueries.sql
+++ sqlQueries.sql
@@ -1,3 +1,5 @@
+---------DELIV 2 QUERRIES---------------
+
 --a)For every year, output the year and the number of publications for said year.
 --then we can groupby result by year and then for each set by year we count nb of elements
 --in the subset, and we output that number and the year
@@ -113,3 +115,39 @@ FROM (
 	ORDER BY popularity DESC
 	LIMIT 3
 ) AS r1;
+
+
+
+-----------DELIV 3 QUERRIES-------------
+--a) Compute the average price per currency of the publications of the most popular title (i.e, the title with
+--most publications overall).
+--b) Output the names of the top ten title series with most awards.
+SELECT t_s.title, 
+	SUM(tit_awrds.nb_awards) AS popularity
+FROM Titles t, Title_Series t_s,
+(SELECT title_id, COUNT(*) AS nb_awards
+FROM title_wins_award
+GROUP BY title_id) AS tit_awrds
+WHERE t.series_id = t_s.id AND t.id = tit_awrds.title_id
+GROUP BY t_s.id
+ORDER BY popularity DESC
+LIMIT 20;
+--c) Output the name of the author who has received the most awards after his/her death.
+
+--d) For a given year, output the three publishers that published the most publications.
+--e) Given an author, compute his/her most reviewed title(s).
+--f) For every language, find the top three title types with most translations.
+--g) For each year, compute the average number of authors per publisher.
+--h) Find the publication series with most titles that have been given awards of “World Fantasy Award”
+--type.
+--i) For every award category, list the names of the three most awarded authors.
+--j) Output the names of all living authors that have published at least one anthology from youngest to
+--oldest.
+--k) Compute the average number of publications per publication series (single result/number expected).
+--l) Find the author who has reviewed the most titles.
+--m) For every language, list the three authors with the most translated titles of “novel” type.
+--n) Order the top ten authors whose publications have the largest pages per dollar ratio (considering all
+--publications of an author that have a dollar price).
+--o) For publications that have been awarded the Nebula award, find the top 10 with the most extensive
+--web presence (i.e, the highest number of author websites, publication websites, publisher websites,
+--publication series websites, and title series websites in total)
