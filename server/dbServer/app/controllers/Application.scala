09b2c66b696d164fa46168b1f187b3e1d8963af8
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-06-07 22:14:26 +0200

    simplify with a for the map on options

--- server/dbServer/app/controllers/Application.scala
+++ server/dbServer/app/controllers/Application.scala
@@ -9,23 +9,23 @@ object DBConf {
 
 import scala.sys.process._ //for executing bash 
 import play.api.mvc._
-import scala.collection.JavaConversions._
 class Application extends Controller {
   import PredefQueries._
   import DBConf._
-  def executeQuery(query: String): Option[String] = 
-    Some{ (s"""echo ${query}""" #| baseConn).!! }
+  def executeQuery(query: String): String = 
+      (s"""echo ${query}""" #| baseConn).!! 
 
   def getQueryOut(qidOpt: Option[String]) = Action { req => 
-    qidOpt.map { qid => 
-      idToQuery.get(qid).flatMap { executeQuery }.map { 
-        queryResult => 
-          Ok(views.html.redirector(queryResult))
-      }.getOrElse{ BadRequest(views.html.redirector("bad request!")) }
-    }.getOrElse{ 
-        BadRequest(
-          views.html.redirector("Provide which query you need! [a-g]2 or [a-o]3")
-        ) 
+    (for {
+      qid <- qidOpt
+      qStr <- idToQuery.get(qid)
+    } yield {
+      try { Ok(views.html.redirector(executeQuery(qStr))) }
+      catch { case e: Exception => Redirect("localost:9000/", 500) }//internal server error
+    }).getOrElse {
+      BadRequest( 
+        views.html.redirector("Provide which query you need! [a-g]2 or [a-o]3")
+      )
     }
   }
 
@@ -339,7 +339,7 @@ object PredefQueries {
 
   val idToQuery = 
     idToQueryBeforeTransform.mapValues{ 
-      _.foldLeft("") { case (str, h) => str + h + "\n" }
+      _.foldLeft("") { _ + _ + "\n" }
     }
     
 }
