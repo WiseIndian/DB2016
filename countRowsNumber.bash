eedb3fd55830403e59ad19f377c4a78901eca85a
Simonchelbc <simon.lebail-collet@epfl.ch>
2016-06-09 15:25:07 +0200

    add function countRowsFromNotTempTables to countRowsNumber.bash

--- countRowsNumber.bash
+++ countRowsNumber.bash
@@ -6,6 +6,13 @@ function sqlConn { # "$@" could be < aSqlScript.sql
         bash sqlConn.bash "$@"
 }
 
+function countRowsFromNotTempTables {
+  sqlQuery="SELECT SUM(Tables.TABLE_ROWS) FROM INFORMATION_SCHEMA.TABLES
+     AS Tables WHERE TABLE_SCHEMA = '$1' AND
+     NOT Tables.TABLE_NAME REGEXP '^.*temp.*$';"
+  echo "$sqlQuery" > tempSqlScript.sql 
+  sqlConn < tempSqlScript.sql
+}
 
 function countRowsFromEveryTables { # can be used as useful feedback
         sqlQuery="SELECT SUM(TABLE_ROWS)
@@ -16,4 +23,3 @@ function countRowsFromEveryTables { # can be used as useful feedback
         #could rm tempSqlScript.sql if we wanted
 }
 
-countRowsFromEveryTables "$1"
