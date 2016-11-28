bd7d492a42501bb0d55ac4d430ac528b2607fc53
Simon Le Bail-Collet <simon.lebail-collet@epfl.ch>
2016-11-28 22:51:07 +0000

    disable only_full_group_by from sql_mode

--- main.bash
+++ main.bash
@@ -28,7 +28,7 @@ fi
 echo "enter root password for mysql server"
 old_sql_mode="$(mysql -u root -h localhost -p <<< "show variables like 'sql_mode'" | sed -n 's/^sql_mode[ \t]*\(.*\)$/\1/p')"
 #get it with mysql + sed
-new_sql_mode='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'
+new_sql_mode='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'
 
 echo "enter sudo password (modifying sql_mode)."
 printedSQL_MODE=0
